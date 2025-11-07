d91fc7f8fc9ad0d0293e73cd932befcb71d4d918
kaelinwanghu <kaelin.wang.hu@gmail.com>
2025-11-07 09:28:36 -0500

    vasaloppet correct solution (basically O(N))

--- vasaloppet.cpp
+++ vasaloppet.cpp
@@ -5,98 +5,133 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
     int n, t, s;
     cin >> n >> t >> s;
     vector<pair<int, int>> ad_breaks(n);
-    int start_time, end_time;
-    int skipped_time = 0, max_skipped_time = 0;
     for (int i = 0; i < n; ++i)
     {
-        cin >> start_time >> end_time;
-        ad_breaks[i] = {start_time, end_time};
+        cin >> ad_breaks[i].first >> ad_breaks[i].second;
     }
-    int current_idx = 0, start = ad_breaks[0].first;
 
-    while (current_idx < n)
+    vector<int> potential_positions;
+    potential_positions.emplace_back(0);
+
+    for (auto [ad_start, ad_end] : ad_breaks)
     {
-        int ad_break_start = ad_breaks[current_idx].first;
-        int ad_break_end = ad_breaks[current_idx].second;
-        int ad_break_time = ad_break_end - ad_break_start;
-        cerr << "current skipped time: " << skipped_time << "\n";
-        if (ad_break_start - start >= s)
+        potential_positions.emplace_back(ad_start);
+        if (ad_end >= s)
         {
-            max_skipped_time = max(max_skipped_time, skipped_time);
-            if (current_idx != 0)
-            {
-                skipped_time -= min(ad_break_end - ad_break_start, s);
-            }
-            current_idx++;
-            if (current_idx < n - 1)
-                start = ad_breaks[current_idx].first;
+            potential_positions.emplace_back(ad_end - s);
         }
-        else if (ad_break_end - start >= s)
+    }
+    sort(potential_positions.begin(), potential_positions.end()); // Note that because the ad breaks are already sorted and disjoint this requires N swaps and should thus be near-linear/linear
+
+    int potential_start = potential_positions[0];
+    int start = potential_start;
+    int end = min(potential_start + s, t);
+
+    int start_idx = 0;
+    while (start_idx < n && ad_breaks[start_idx].second <= start)
+        ++start_idx;
+    int temp = start_idx;
+
+    int end_idx = 0;
+
+    int ad_time = 0;
+    while (temp < n && ad_breaks[temp].first < end)
+    {
+        int overlap_l = max(start, ad_breaks[temp].first);
+        int overlap_r = min(end, ad_breaks[temp].second);
+        ad_time += overlap_r - overlap_l;
+
+        if (ad_breaks[temp].second <= end)
         {
-            skipped_time += min(ad_break_time, s);
-            max_skipped_time = max(max_skipped_time, skipped_time);
-            if (current_idx != 0)
-            {
-                skipped_time -= min((ad_break_end - ad_break_start), s);
-            }
-            current_idx++;
-            if (current_idx < n - 1)
-                start = ad_breaks[current_idx].first;
+            ++temp;
         }
         else
         {
-            skipped_time += ad_break_time;
-            current_idx++;
+            break;
         }
     }
 
-    reverse(ad_breaks.begin(), ad_breaks.end());
-    for (int i = 0; i < n; ++i)
+    end_idx = temp;
+    int max_ad_time = ad_time;
+
+    int positions_size = potential_positions.size();
+    for (int i = 1; i < positions_size; ++i)
     {
-        ad_breaks[i] = {t - ad_breaks[i].second, t - ad_breaks[i].first};
+        int next_start = potential_positions[i];
+        int dx = next_start - potential_start;
+        {
+            int seg_start = start;
+            int seg_end = start + dx;
+
+            while (start_idx < n && ad_breaks[start_idx].second <= seg_start)
+            {
+                ++start_idx;
+            }
+
+            int p = start_idx;
+            while (p < n && ad_breaks[p].first < seg_end)
+            {
+                int overlap_start = max(seg_start, ad_breaks[p].first);
+                int overlap_end = min(seg_end, ad_breaks[p].second);
+                if (overlap_end > overlap_start)
+                {
+                    ad_time -= (overlap_end - overlap_start);
+                }
+
+                if (ad_breaks[p].second <= seg_end)
+                {
+                    ++p;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            start_idx = p;
+        }
+
+        potential_start = next_start;
+        start += dx;
+        end = min(end + dx, t);
+
+        {
+            int seg_start = end - dx;
+            int seg_end = end;
+
+            while (end_idx < n && ad_breaks[end_idx].second <= seg_start)
+            {
+                ++end_idx;
+            }
+
+            int q = end_idx;
+            while (q < n && ad_breaks[q].first < seg_end)
+            {
+                int add_l = max(seg_start, ad_breaks[q].first);
+                int add_r = min(seg_end, ad_breaks[q].second);
+                if (add_r > add_l)
+                {
+                    ad_time += (add_r - add_l);
+                }
+
+                if (ad_breaks[q].second <= seg_end)
+                {
+                    ++q;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            end_idx = q;
+        }
+
+        max_ad_time = max(max_ad_time, ad_time);
     }
 
-    current_idx = 0;
-    start = ad_breaks[0].first;
-    skipped_time = 0;
-    while (current_idx < n)
-    {
-        int ad_break_start = ad_breaks[current_idx].first;
-        int ad_break_end = ad_breaks[current_idx].second;
-        int ad_break_time = ad_break_end - ad_break_start;
-        cerr << "current skipped time: " << skipped_time << "\n";
-        if (ad_break_start - start >= s)
-        {
-            max_skipped_time = max(max_skipped_time, skipped_time);
-            if (current_idx != 0)
-            {
-                skipped_time -= min(ad_break_end - ad_break_start, s);
-            }
-            current_idx++;
-            if (current_idx < n - 1)
-                start = ad_breaks[current_idx].first;
-        }
-        else if (ad_break_end - start >= s)
-        {
-            skipped_time += min(ad_break_time, s);
-            max_skipped_time = max(max_skipped_time, skipped_time);
-            if (current_idx != 0)
-            {
-                skipped_time -= min((ad_break_end - ad_break_start), s);
-            }
-            current_idx++;
-            if (current_idx < n - 1)
-                start = ad_breaks[current_idx].first;
-        }
-        else
-        {
-            skipped_time += ad_break_time;
-            current_idx++;
-        }
-    }
-
-    cout << (s - max_skipped_time) << "\n";
+    cout << (s - max_ad_time) << "\n";
+    return 0;
 }
